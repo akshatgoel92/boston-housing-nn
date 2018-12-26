c8bf4480d9fff368518d8f0acd53beec9051c8f0
akshatgoel92 <akshatgoel92@hotmail.com>
2018-12-26 15:34:40 +0530

    Add debugged network

--- nn_scratch.py
+++ nn_scratch.py
@@ -4,10 +4,15 @@
 # Description: Basic Neural Net
 #--------------------------------#
 
-# Import libraries 
+# Import libraries
+import numpy as np 
 import random 
 import os 
-import numpy as np
+
+# Import data
+from sklearn.datasets import load_boston
+from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import train_test_split
 
 # Artificial Neural Net
 # This Python class implements an artificial neural net
@@ -16,7 +21,6 @@ import numpy as np
 # layers: Length of the arch vector which tells us the no. of hidden layers
 # weights: Impact of each input on neuron - initialized using a standard normal distribution
 # learning_rate: Hyperparameter that controls size of gradient decent jumps at each iteration
-
 class NeuralNet(object):
 
     # Divide into train and test data
@@ -24,32 +28,34 @@ class NeuralNet(object):
     # Initialize weights
     # Intialize no. of layers
     # Define architecture 
-    def __init__(self, train, test, arch, rate, iterations): 
+    def __init__(self, X_train, y_train, X_test, y_test, arch, rate, iterations): 
 
         # Separate training labels
         # Separate training features
-        self.train_labels = train[-1]
-        self.train_features = train[:-1]
+        # This assumes that the labels are the last column
+        self.train_labels = y_train
+        self.train_features = X_train
 
         # Separate test labels
         # Separate test features
-        self.test_labels = test[-1]
-        self.test_features = train[:-1]
+        # This is the same as above but for the test data
+        self.test_labels = X_test
+        self.test_features = y_test
         
         # Store training data dimensions
         # n_x: No. of observations
         # n_y: No. of observations 
         # n_m: No. of features
-        self.n_x = nrow(training)
-        self.n_y = nrow(training)
-        self.n_m = ncol(training)
+        self.n_x = len(X_train)
+        self.n_y = len(y_train)
+        self.n_m = len(X_train[0])
         
         # Need to insert dimension of input layer
         # User inputs only architecture of hidden layers
         # Calculate the no. of layers only after this is done
-        # This is so that we include the input layer in the total layers 
-        self.arch = arch.insert(0, n_x)
-        self.layers = range(1, len(arch) + 1)
+        # This is so that we include the input layer in the total layers
+        self.arch = arch
+        self.layers = range(1, len(arch))
         
         # Store the learning rate
         # Store the number of iterations
@@ -58,16 +64,23 @@ class NeuralNet(object):
     
     # Input: Vector with no. of neurons in each layer
     # Output: Vector of initialized weights and biases
-    def get_inital_weights(self):
+    def get_initial_weights(self):
         
         # This contains data for:
         # Architecture
         # Layers 
+
         arch = self.arch
         layers = self.layers
 
+        print(self.arch)
+        print(arch)
+        print(layers)
+
+        # Initialize weights according to a standard normal distribution
+        # Store initialization in a dictionary where the key is the layer number
         W = {l: np.random.randn(arch[l], arch[l - 1]) for l in layers}
-        b = {l: np.random.randn(arch[l-1], 1) for l in layers} 
+        b = {l: np.random.randn(arch[l], 1) for l in layers} 
         
         return(W, b)
 
@@ -80,7 +93,7 @@ class NeuralNet(object):
         # Store the layers 
         # iterable
         layers = self.layers 
-        L = len(layers)
+        L = layers[-1]
 
         # This is the initial layer
         # where to store information 
@@ -89,40 +102,33 @@ class NeuralNet(object):
         
         # Now iterate over layers
         for l in layers:
-            
-            a = A[l-1]
-            w = W[l)]
-            b = B[l]
-            
-            Z[l] = np.sum(w @ a, b)
-            A[l] = self.sigmoid(Z[l]))
+
+        	Z[l] = np.add(W[l] @ A[l-1], B[l])
+        	A[l] = self.sigmoid(Z[l])
 
         # Store the 'error' term from the last layer
-        dZL = np.dot(A[L] - Y, self.sigmoid_prime(Z[L])) 
-
-        return(dZL, A, W, b) 
+        dZL = self.sigmoid_prime(Z[L]) * (A[L] - Y)
+                
+        return(dZL, A, W, B, Z) 
 
     # Backward propogation
     # Propagate the output layer's error backward
     # Calculations are given by back-propagation equations
-    # This allows us to see how cost is impacted by adjusting weights 
-    # in previous layers 
-    def backward_propogation(self, dZL, dAL):
+    # This allows us to see how cost is impacted by adjusting weights in previous layers 
+    def backward_propogation(self, dZL, A, W, b, Z):
 
-        # Initalize dictionaries to 
-        # store the derivatives
-        L = len(self.arch)
+        # Initalize dictionaries to store the derivatives
+        L = len(self.arch) - 1
         
-        dA = {L: dAL}
         dZ = {L: dZL}
         dW = {}
         db = {}
         
-        for layer in reversed(range(2, L)):
+        for l in reversed(range(1, L + 1)):
 
-            dZ[l-1] = np.dot(W[l].T @ dZ[l], self.sigmoid_prime(Z[l-1])) 
-            dW[l-1] = dZ[l] @ A[l].T
-            db[l-1] = db[l]
+            dZ[l-1] = (W[l].T @ dZ[l]) * self.sigmoid_prime(A[l-1]) 
+            dW[l] = dZ[l] @ A[l-1].T
+            db[l] = dZ[l]
 
         return(dW, db)
          
@@ -151,14 +157,6 @@ class NeuralNet(object):
 
         return(self.sigmoid(z)*(1 - self.sigmoid(z)))
 
-    # Create mini batch of given size 
-    # Use this for stochastic gradient descent
-    # Mini-batches save time by learning from a sample
-    # of training examples at each gradient descent iteration 
-    def get_mini_batch(self):
-        
-        pass
-
     # Gradient descent
     # This function updates weights according to 
     # stochastic gradient descent formula given a 
@@ -168,60 +166,89 @@ class NeuralNet(object):
     # Then return the weights  
     # Input: Weights, learning rate, current gradient
     # Output: Updated weights from 1 iteration of gradient descent
-    def gradient_descent(self, weights, rate, dW):
+    def gradient_descent(self, W, b, dW, db, rate):
 
-        return([w - rate*dW['dW' + str(layer)] 
-                for weight in layer 
-                for layer in weights])
+    	W = {l: np.subtract(W[l], rate*dW[l]) for l in W.keys()}
+    	b = {l: np.subtract(b[l], rate*db[l]) for l in b.keys()}
 
-    # Input: -
+    	return(W, b)
+
+    # Input: Training and test data-sets
     # Output: Neural network training progress for every 100 SGD iterations
     def train(self):
 
-        w, b = self.initialize_weights()
-        
-        for i in self.iterations:
-            
-            activations = forward_propagation(self.train, w, b)
-            gradients = backward_propogation(gradients)
-            weights = gradient_descent(weights, self.learning_rate, gradients)
-            
-            train_predictions = 
-            test_predictions =  
-            
-            train_cost = get_cost(predictions)
-            test_cost = get_cost(predictions)
 
+        w, b = self.get_initial_weights()
+        L = len(self.arch) - 1
+
+        for iteration in range(self.iterations):
+            
+            dZL, A, W, b, Z = self.forward_propogation(self.train_features, self.train_labels, w, b)
+            dW, db = self.backward_propogation(dZL, A, W, b, Z)
+            W, b = self.gradient_descent(W, b, dW, db, self.rate)
+                        
+            train_preds = A[L]
+            train_cost = self.get_cost(train_preds, self.train_labels)
+           
             if iteration % 100 == 0:
                 
-                print("The training cost is: " + str(cost))
-                print("The test cost is: " + str(cost))
+                print("The training cost is: " + str(train_cost))
 
-                print("The training accuracy is: " + str(accuracy))
-                print("The test accuracy is: " + str(accuracy))
+    # This function implements check shapes            
+    def check_shapes(dZL, A, W, b, Z):
+
+        print('dZL shape:')
+        print(dZL.shape)
+        
+        print('W shape:')
+        print({key: W[key].shape for key in W.keys()})
+        
+        print('A shape:')
+        print({key: A[key].shape for key in A.keys()})
+        
+        print('b shape:')
+        print({key: b[key].shape for key in b.keys()})
+        
+        print('Z shape:')
+        print({key: Z[key].shape for key in Z.keys()})
 
 
+# Input: Load and process data
+def process_data(X):
+
+ 	transformer = RobustScaler().fit(X)
+ 	return(transformer.transform(X))
+
+                
 # Now we will execute the script
 # We will load the data-set 
 # Next we will initialize a neural net object
 # Then we will train the neural network 
 # Then we will make predictions
-if __name__ == main():
+if __name__ == '__main__':
     
+
     # Set the parameters here
-    data = train_test_split.TrainTestSplit()
-    train = 
-    test = 
+    X, y = load_boston(return_X_y = True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+    # Prepare dimensions of test data
+    y_train = y_train.reshape(len(y_train), 1)
+    y_test = y_test.reshape(len(y_test), 1)
+
+    # Process data
+    X_train, X_test = process_data(X_train), process_data(X_test), 
+    y_train, y_test = process_data(y_train), process_data(y_test)
     
-    # Set the architecture 
-    # Set the lLearning rate 
-    # 
-    arch = [4,3,2,1]
+    # Set the architecture
+    # Add in the input layer
+    # Set the learning rate 
+    # Set the number of iterations
+    arch = [len(X_train), 600, 575, 500, 450, 400, 350, len(y_train)]
     rate = 0.6
+    iterations = 50000
     
     # Create a neural net object
     # Train this object 
-    nn = NeuralNet(train, test, arch, rate)
+    nn = NeuralNet(X_train, y_train, X_test, y_test, arch, rate, iterations)
     nn.train()
-
-
